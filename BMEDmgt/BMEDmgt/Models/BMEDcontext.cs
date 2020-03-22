249106c0e505a352674ea07e7cdf0c3a70526bf0
healthcon healthcon <healthcon@outlook.com>
2020-03-22 23:40:26 +0800

    update connection

--- BMEDmgt/BMEDmgt/Models/BMEDcontext.cs
+++ BMEDmgt/BMEDmgt/Models/BMEDcontext.cs
@@ -9,7 +9,7 @@ namespace BMEDmgt.Models
     public class BMEDcontext: DbContext
     {
         public BMEDcontext()
-        :base("AzureConnection") { }//AzureConnection;BMEDconnection;TSGHConnection
+        :base("BMEDconnection") { }//AzureConnection;BMEDconnection;TSGHConnection
 
         public DbSet<AppUser> AppUsers { get; set; }
         public DbSet<CustOrgan> CustOrgans { get; set; }
