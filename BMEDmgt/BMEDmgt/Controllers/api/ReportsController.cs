3f4f5ced64c15b915d36a7287e4b39491126f8f1
healthcon <healthcon@outlook.com>
2020-05-08 11:18:40 +0800

    1090508

---
+++ BMEDmgt/BMEDmgt/Controllers/api/ReportsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BMEDmgt.Areas.MedEngMgt.Models;
+using BMEDmgt.Models;
+
+namespace BMEDmgt.Controllers.api
+{
+    public class ReportsController : ApiController
+    {
+        private BMEDcontext db = new BMEDcontext();
+
+        public IQueryable<RpKpHistoryVModel> RpKpHistory(string assetNo)
+        {
+            List<RpKpHistoryVModel> sv = new List<RpKpHistoryVModel>();
+            List<RpKpHistoryVModel> sv2 = new List<RpKpHistoryVModel>();
+            if (string.IsNullOrEmpty(assetNo))
+                return sv.AsQueryable();
+            var ss = new[] { "?", "2" };
+            sv = db.Assets.Where(a => a.AssetNo == assetNo)
+                .Join(db.Repairs, a => a.AssetNo, r => r.AssetNo,
+                (a, r) => new
+                {
+                    DocType = "請修",
+                    DocId = r.DocId,
+                    AssetNo = a.AssetNo,
+                    AssetName = a.Cname,
+                    ApplyDate = r.ApplyDate,
+                    TroubleDes = r.TroubleDes
+                }).Join(db.RepairFlows.Where(f => ss.Contains(f.Status)), r => r.DocId, f => f.DocId,
+                    (r, f) => r).Join(db.RepairDtls, r => r.DocId, d => d.DocId,
+                (r, d) => new
+                {
+                    DocType = "請修",
+                    DocId = r.DocId,
+                    AssetNo = r.AssetNo,
+                    AssetName = r.AssetName,
+                    ApplyDate = r.ApplyDate,
+                    TroubleDes = r.TroubleDes,
+                    DealDes = d.DealDes,
+                    EndDate = d.EndDate,
+                    Hours = d.Hour,
+                    Cost = d.Cost
+                }).Join(db.RepairEmps.Join(db.AppUsers, r => r.UserId, a => a.Id,
+                (r, a) => new
+                {
+                    DocId = r.DocId,
+                    UserName = a.FullName
+                }), d => d.DocId, e => e.DocId,
+                (d, e) => new RpKpHistoryVModel
+                {
+                    DocType = "請修",
+                    DocId = d.DocId,
+                    AssetNo = d.AssetNo,
+                    AssetName = d.AssetName,
+                    ApplyDate = d.ApplyDate.Value,
+                    TroubleDes = d.TroubleDes,
+                    DealDes = d.DealDes,
+                    EndDate = d.EndDate,
+                    Hours = d.Hours,
+                    Cost = d.Cost,
+                    EngName = e.UserName
+                }).ToList();
+            //
+            sv2 = db.Assets.Where(a => a.AssetNo == assetNo)
+                .Join(db.Keeps, a => a.AssetNo, r => r.AssetNo,
+                (a, r) => new
+                {
+                    DocType = "保養",
+                    DocId = r.DocId,
+                    AssetNo = a.AssetNo,
+                    AssetName = a.Cname,
+                    ApplyDate = r.SentDate,
+                    TroubleDes = ""
+                }).Join(db.KeepFlows.Where(f => ss.Contains(f.Status)), r => r.DocId, f => f.DocId,
+                    (r, f) => r).Join(db.KeepDtls, r => r.DocId, d => d.DocId,
+                (r, d) => new
+                {
+                    DocType = r.DocType,
+                    DocId = r.DocId,
+                    AssetNo = r.AssetNo,
+                    AssetName = r.AssetName,
+                    ApplyDate = r.ApplyDate,
+                    TroubleDes = r.TroubleDes,
+                    DealDes = d.Result,
+                    EndDate = d.EndDate,
+                    Hours = d.Hours,
+                    Cost = d.Cost
+                }).Join(db.KeepEmps.Join(db.AppUsers, r => r.UserId, a => a.Id,
+                (r, a) => new
+                {
+                    DocId = r.DocId,
+                    UserName = a.FullName
+                }), d => d.DocId, e => e.DocId,
+                (d, e) => new RpKpHistoryVModel
+                {
+                    DocType = d.DocType,
+                    DocId = d.DocId,
+                    AssetNo = d.AssetNo,
+                    AssetName = d.AssetName,
+                    ApplyDate = d.ApplyDate.Value,
+                    TroubleDes = d.TroubleDes,
+                    DealDes = d.DealDes,
+                    EndDate = d.EndDate,
+                    Hours = d.Hours,
+                    Cost = d.Cost,
+                    EngName = e.UserName
+                }).ToList();
+            sv.AddRange(sv2);
+
+            return sv.AsQueryable();
+
+        }
+    }
+}
